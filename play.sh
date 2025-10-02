192603bc53488de1e89ca4439947458025ce7e7d
Daniel Mewes <daniel.mewes@imbue.com>
2025-10-02 13:45:12 -0700

    Suppress warnings in play.sh

--- play.sh
+++ play.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Launcher script for Lists and Lists
 
-if command -v sbcl &> /dev/null; then
-    sbcl --noinform --quit --load lists-and-lists.lisp --eval "(lists-and-lists:play-game)"
-elif command -v ccl &> /dev/null; then
+if command -v sbcl > /dev/null; then
+    sbcl --noinform --eval "(declaim (sb-ext:muffle-conditions style-warning))" --quit --load lists-and-lists.lisp --eval "(lists-and-lists:play-game)"
+elif command -v ccl > /dev/null; then
     ccl --load lists-and-lists.lisp --eval "(lists-and-lists:play-game)" --eval "(quit)"
-elif command -v clisp &> /dev/null; then
+elif command -v clisp > /dev/null; then
     clisp -q -i lists-and-lists.lisp -x "(lists-and-lists:play-game)"
 else
     echo "No Common Lisp implementation found."
