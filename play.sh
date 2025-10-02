cb86a587c67151cef3371e0b8772e6fd5fd9d31d
Daniel Mewes <daniel.mewes@imbue.com>
2025-10-02 13:19:52 -0700

    Quite play.sh after playing

--- play.sh
+++ play.sh
@@ -2,9 +2,9 @@
 # Launcher script for Lists and Lists
 
 if command -v sbcl &> /dev/null; then
-    sbcl --noinform --load lists-and-lists.lisp --eval "(lists-and-lists:play-game)"
+    sbcl --noinform --quit --load lists-and-lists.lisp --eval "(lists-and-lists:play-game)"
 elif command -v ccl &> /dev/null; then
-    ccl --load lists-and-lists.lisp --eval "(lists-and-lists:play-game)"
+    ccl --load lists-and-lists.lisp --eval "(lists-and-lists:play-game)" --eval "(quit)"
 elif command -v clisp &> /dev/null; then
     clisp -q -i lists-and-lists.lisp -x "(lists-and-lists:play-game)"
 else
